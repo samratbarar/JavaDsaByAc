fe8d49c6988258389bcb204b185db694567a0949
samratbarar <samratbarar7707@gmail.com>
2026-06-16 20:20:23 +0530

    Hello world

---
+++ Lecture_1/helloWorld.java
@@ -0,0 +1,11 @@
+package JavaDsaByAc.Lecture_1;
+
+import java.util.Scanner;
+
+public class helloWorld {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Hello World");
+    }
+}
