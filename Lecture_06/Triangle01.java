8c4fc3ac37749bbf3085178b6911ca8817020bd6
samratbarar <samratbarar7707@gmail.com>
2026-06-21 22:37:58 +0530

    01 triangle

---
+++ Lecture_06/Triangle01.java
@@ -0,0 +1,27 @@
+package JavaDsaByAc.Lecture_06;
+
+import java.util.*;
+
+// Program to print pattern of 01 triangle
+
+public class Triangle01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the height of triangle ");
+        int height = sc.nextInt();
+
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            } 
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
