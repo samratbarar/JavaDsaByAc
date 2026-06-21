844e8100409bfd59e90589a688cdd49b4b146457
samratbarar <samratbarar7707@gmail.com>
2026-06-21 15:56:57 +0530

    Basic

---
+++ Lecture_03/allQues.java
@@ -0,0 +1,15 @@
+package JavaDsaByAc.Lecture_03;
+
+import java.util.*;
+
+// just implementation of operators
+
+public class allQues {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // implementation
+
+        sc.close();
+    }
+}
