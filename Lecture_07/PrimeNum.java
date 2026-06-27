88a92126575de382c9e1489382634e37a61877ec
samratbarar <samratbarar7707@gmail.com>
2026-06-27 13:07:23 +0530

    Prime number checker and printer in range

--- Lecture_07/PrimeNum.java
+++ Lecture_07/PrimeNum.java
@@ -8,7 +8,7 @@ public class PrimeNum {
             return false;
         }
 
-        for (int i = 2; i < Math.sqrt(num); i++) {
+        for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
                 return false;
             }
@@ -22,12 +22,15 @@ public class PrimeNum {
             System.out.println("Enter valid range");
         }
 
+        System.out.println("Prime numbers up to " + range + ":");
         for (int i = 2; i <= range; i++) {
             if (isPrime(i)) {
-                System.out.println(i + " ");
+                System.out.print(i + " ");
             }
         }
+        System.out.println();
     }
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
