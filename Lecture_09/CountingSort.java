b3a89d73d8f9480fa2da9c66738fe50d3cf62999
samratbarar <samratbarar7707@gmail.com>
2026-06-30 22:08:20 +0530

    Counting sort

---
+++ Lecture_09/CountingSort.java
@@ -0,0 +1,48 @@
+package JavaDsaByAc.Lecture_09;
+
+import java.util.*;
+
+public class CountingSort {
+    public static void inputArray(int[] arr, Scanner sc) {
+        System.out.println("Enter the elements of array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements of array");
+        int[] arr = new int[sc.nextInt()];
+
+        inputArray(arr, sc);
+
+        int largest = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        int[] freq = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            freq[arr[i]]++;
+        }
+
+        // sorting for positive and small numbers 
+        int j = 0;
+        for (int i = 0; i < freq.length; i++) {
+            while (freq[i] > 0) {
+                arr[j] = i;
+                j++;
+                freq[i]--;
+            }
+        }
+
+        System.out.println("Elements of sorted array are: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
