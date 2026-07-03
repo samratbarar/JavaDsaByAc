7f3d8704fab93047a71501251393721e46457086
samratbarar <samratbarar7707@gmail.com>
2026-07-03 15:51:57 +0530

    Num is power of 2 or not

---
+++ Lecture_12/IsPowerOf2.java
@@ -0,0 +1,20 @@
+package JavaDsaByAc.Lecture_12;
+
+import java.util.*;
+
+public class IsPowerOf2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int num = sc.nextInt();
+
+        if (num > 0 && (num & (num - 1)) == 0) {
+            System.out.println(num + " is power of 2");
+        } else {
+            System.out.println(num + " is not power of 2");
+        }
+
+        sc.close();
+    }
+}
