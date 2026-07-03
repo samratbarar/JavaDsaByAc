1cfd1c2954c3e717cbb20d6e90b974c8873e8b6b
samratbarar <samratbarar7707@gmail.com>
2026-07-03 14:34:03 +0530

    Compression of string

---
+++ Lecture_11/StringCompression.java
@@ -0,0 +1,33 @@
+package JavaDsaByAc.Lecture_11;
+
+import java.util.*;
+
+public class StringCompression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the string: ");
+        String str = sc.nextLine();
+
+        StringBuilder compressed = new StringBuilder("");
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (i + 1 < str.length() && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                compressed.append(str.charAt(i));
+
+                if (count > 1) {
+                    compressed.append(count);
+                }
+
+                count = 1;
+            }
+        }
+
+        System.out.println("The compressed string is " + compressed);
+
+        sc.close();
+    }
+}
