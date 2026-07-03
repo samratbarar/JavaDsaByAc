da1a8069204a2262dc1535d3da57dfeca8d897aa
samratbarar <samratbarar7707@gmail.com>
2026-07-03 16:06:37 +0530

    Binary Exponatition

---
+++ Lecture_12/FastExpo.java
@@ -0,0 +1,26 @@
+package JavaDsaByAc.Lecture_12;
+
+import java.util.*;
+
+public class FastExpo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the base and positive power");
+        int base = sc.nextInt(), power = sc.nextInt();
+
+        int ans = 1;
+        while (power > 0) {
+            if ((power & 1) != 0) {
+                ans *= base;
+            }
+
+            base *= base;
+            power = power >> 1;
+        }
+
+        System.out.println("The answer is " + ans);
+
+        sc.close();
+    }
+}
