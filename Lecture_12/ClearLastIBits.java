4d5fffffa816d412f1096709d6ffd5c0e8e266e8
samratbarar <samratbarar7707@gmail.com>
2026-07-03 15:25:34 +0530

    Clear all bits upto ith bit

---
+++ Lecture_12/ClearLastIBits.java
@@ -0,0 +1,24 @@
+package JavaDsaByAc.Lecture_12;
+
+import java.util.Scanner;
+
+public class ClearLastIBits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number ");
+        int num = sc.nextInt();
+
+        System.out.println("Enter the value of i ");
+        int i = sc.nextInt();
+
+        // for (int j = 0; j < i; j++) { --> my approach
+        //     num = num & ~(1 << j);
+        // }
+
+        num = num & ((-1) << i);
+        System.out.println(num);
+
+        sc.close();
+    }
+}
