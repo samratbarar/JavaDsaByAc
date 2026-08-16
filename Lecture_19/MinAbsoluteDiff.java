3a561ebc31463dc694dbc3200cbb4fde7ed3d417
samratbarar <samratbarar7707@gmail.com>
2026-08-16 23:06:45 +0530

    Minimum absolute difference

---
+++ Lecture_19/MinAbsoluteDiff.java
@@ -0,0 +1,21 @@
+package Lecture_19;
+
+import java.util.*;
+
+public class MinAbsoluteDiff {
+    public static void main(String[] args) {
+        int[] A = {4, 1, 8, 7};
+        int[] B = {2, 3, 6, 5};
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int minDiff = 0;
+
+        for (int i = 0; i < B.length; i++) {
+            minDiff += Math.abs(A[i] - B[i]);
+        }
+
+        System.out.println("Minimum Absolute difference is " + minDiff);
+    }
+}
