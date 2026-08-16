ce2a09f533dcbb24a158c0c67dff36d2a909589b
samratbarar <samratbarar7707@gmail.com>
2026-08-16 19:59:58 +0530

    Get maximum total value in knapsack

--- Lecture_19/FractionalKnapsack.java
+++ Lecture_19/FractionalKnapsack.java
@@ -1,5 +1,37 @@
 package Lecture_19;
 
+import java.util.*;
+
 public class FractionalKnapsack {
-    
+    public static void main(String[] args) {
+        int[] val = {60, 100, 120};
+        int[] weight = {10, 20, 30};
+        int W = 50;
+
+        double[][] ratio = new double[val.length][2];
+
+        for (int i = 0; i < val.length; i++) {
+            ratio[i][0] = i;
+            ratio[i][1] = (double)val[i] / weight[i];
+        }
+
+        Arrays.sort(ratio, Comparator.comparingDouble(o -> o[1]));
+
+        int capacity = W;
+        int finValue = 0;
+
+        for (int i = ratio.length - 1; i >= 0; i--) {
+            int idx = (int)ratio[i][0];
+            if (capacity >= weight[idx]) {
+                finValue += val[idx];
+                capacity -= weight[idx];
+            } else {
+                finValue += (ratio[i][1] * capacity);
+                capacity = 0;
+                break;
+            }
+        }
+
+        System.out.println("Final value is " + finValue);
+    }
 }
