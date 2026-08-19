1b22cab13f7b709c615085413165b21e1a5f8921
samratbarar <samratbarar7707@gmail.com>
2026-08-19 22:55:43 +0530

    Next Greater Element

---
+++ Lecture_21/NextGreaterEle.java
@@ -0,0 +1,27 @@
+package Lecture_21;
+
+import java.util.*;
+
+public class NextGreaterEle {
+    public static void main(String[] args) {
+        int[] arr = {6, 8, 0, 1, 3};
+        int[] nextGreater = new int[arr.length];
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int curr = arr[i];
+
+            while (!s.isEmpty() && s.peek() <= curr) {
+                s.pop();
+            }
+
+            if (s.isEmpty()) {
+                nextGreater[i] = -1;
+            } else {
+                nextGreater[i] = s.peek();
+            }
+
+            s.push(curr);
+        }
+    }
+}
